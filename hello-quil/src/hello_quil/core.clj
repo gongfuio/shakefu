97d0d9a8d381837d05819522a07a57c113fafbce
david hodgetts <david.demainlalune@gmail.com>
2014-10-16 16:32:31 +0200

    simplify tweet connection example

--- hello-quil/src/hello_quil/core.clj
+++ hello-quil/src/hello_quil/core.clj
@@ -10,45 +10,25 @@
 (def thread-pool (at-at/mk-pool))
 
 
-; setup connection to tweets
-; tweets are stored in a queue
-(def tweets (atom []))
-
-(defn tweet-handler
-  [http-body]
-  (let [tweet (parse-string http-body keyword)
-		max-queue-size 4]
-   (if (not (contains? @tweets tweet))
-	 (do
-	 	(swap! tweets conj tweet)
-	   	(if (> (count @tweets) max-queue-size) (swap! tweets subvec 1))))))
-
-(def connection (tweet/connect-tweet-server thread-pool 4000 tweet-handler))
-
-;(tweet/stop-connection connection)
-
-
-; setup state for the tweet to be displayed by quil
-; this tweet is updated every n seconds with at-at schedule
 (def tweet (atom {:id_str ""
-				          :text ""
-				          :profile-image-url nil}))
-
-(defn update-tweet
-  []
-  (let [latest-tweet (last @tweets)
-		profile-image-url #(-> % :user :profile_image_url)]
-	  (if (not (= (:id_str latest-tweet) (:id_str @tweet))); only update if there is a new tweet (not smart)
-	    (reset! tweet {
-					           :id_str (:id_str latest-tweet)
-					           :text (:text latest-tweet)
-					           :profile-image-url (profile-image-url latest-tweet)}))))
-
-(def update-tweet-schedule (at-at/every 8000 update-tweet thread-pool))
-
-;(at-at/stop update-tweet-schedule)
+				  :text ""
+				  :profile-image-url nil}))
 
 @tweet
+
+
+(defn update-tweet
+  [http-body]
+  (let [new-tweet (parse-string http-body keyword)
+		profile-image-url #(-> % :user :profile_image_url)]
+	  (if (not (= (:id_str new-tweet) (:id_str @tweet))); only update if there is a new tweet
+	    (reset! tweet {:id_str (:id_str new-tweet)
+					   :text (:text new-tweet)
+					   :profile-image-url (profile-image-url new-tweet)}))))
+
+(def connection (tweet/connect-tweet-server thread-pool 4000 update-tweet))
+
+;(tweet/stop-connection connection)
 
 (defn image-available?
   "checks if requested PImage is available"
@@ -71,12 +51,12 @@
   ; Update sketch state by changing circle color and position.
   (let [old_id_str (:old_id_str state)
 		new_id_str (:id_str @tweet)
-		should-update-tweet (not (= old_id_str new_id_str))]
+		new-tweet? (not (= old_id_str new_id_str))]
   {:color (mod (+ (:color state) 0.7) 255)
    :angle (+ (:angle state) 0.1)
-   :old_id_str (if should-update-tweet new_id_str old_id_str)
+   :old_id_str (if new-tweet? new_id_str old_id_str)
    :text (:text @tweet)
-   :profile-image (if should-update-tweet (-> @tweet :profile-image-url q/request-image) (:profile-image state))}))
+   :profile-image (if new-tweet? (-> @tweet :profile-image-url q/request-image) (:profile-image state))}))
 
 (defn draw [state]
   ; Clear the sketch by filling it with light-grey color.
