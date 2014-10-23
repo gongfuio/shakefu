6428dbe58ac2677e0f7deff254213639ccf78e08
Olivier Lange <olange@petit-atelier.ch>
2014-10-23 12:01:38 +0200

    Added Quil project skeleton to visualize tweets as a force-directed graph

---
+++ girafe/README.md
@@ -0,0 +1,12 @@
+# Girafe
+
+A Quil sketch designed to ... well, that part is up to you.
+
+## Usage
+
+LightTable - open `core.clj` and press `Ctrl+Shift+Enter` to evaluate the file.
+
+Emacs - run cider, open `core.clj` and press `C-c C-k` to evaluate the file.
+
+REPL - run `(require 'girafe.core)`.
+
---
+++ girafe/project.clj
@@ -0,0 +1,9 @@
+(defproject girafe "0.1.0-SNAPSHOT"
+  :description "A visualization of tweets and their authors as a force-directed graph"
+  :url "https://github.com/gongfuio/shakefu/girafe"
+  :license {:name "Creative Commons Attribution-ShareAlike 4.0 International"
+            :url "http://creativecommons.org/licenses/by-sa/4.0/"}
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+    [quil "2.2.2"]]
+  :main girafe.core)
