de72dfca5c45ae8aa07c3bc50b76bd03adf2c16c
Bertrand Cachet <bertrand.cachet@gmail.com>
2014-10-24 23:39:18 +0200

    REF: Implementation based on core.async

--- hello-tweet/project.clj
+++ hello-tweet/project.clj
@@ -5,9 +5,10 @@
     :name "Eclipse Public License"
     :url "http://www.eclipse.org/legal/epl-v10.html" }
   :dependencies [
-    [org.clojure/clojure "1.6.0"]
-    [quil "2.2.2"]
-	[http-kit "2.1.16"]
-    [cheshire "5.3.1"]
-	[overtone/at-at "1.2.0"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [quil "2.2.2"]
+                 [http-kit "2.1.16"]
+                 [cheshire "5.3.1"]
+                 [overtone/at-at "1.2.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :main hello-tweet.core)
--- hello-tweet/src/hello_tweet/core.clj
+++ hello-tweet/src/hello_tweet/core.clj
@@ -1,84 +1,46 @@
 (ns hello-tweet.core
   (:require [quil.core :as q]
             [quil.middleware :as m]
-			[hello-tweet.tweet-connection :as tweet]
-			[overtone.at-at :as at-at]))
+            [hello-tweet.tweet-connection :as tweet]
+            [overtone.at-at :as at-at]
+            [clojure.core.async :refer [<!! >! chan go]]))
 
-; pool of threads for at-at
-(def thread-pool (at-at/mk-pool))
+(defn img-loaded? [image]
+  (and image
+       (> (.-width image) 0)))
 
-; mutable state holding the fields we wish to display
-(def tweet (atom {:id_str ""
-				  :text ""
-				  :screen-name ""
-				  :profile-image-url nil}))
+(let [tweets (chan)]
 
-(defn update-tweet
-	"function called on reception of a new tweet.
- 	new-tweet is a map of the json structure returned by twitter
- 	keys as :keywords with js syntax (:screen_name instead of idiomatic :screen-name)"
-  [new-tweet]
-	  (if (not (= (:id_str new-tweet) (:id_str @tweet))); only update if there is a new tweet
-	    (reset! tweet {:id_str (:id_str new-tweet)
-					   :text (:text new-tweet)
-					   :screen-name (get-in new-tweet [:user :screen_name])
-					   :profile-image-url (get-in new-tweet [:user :profile_image_url])})))
+  (defn- start-connection []
+    (let [thread-pool (at-at/mk-pool)
+          update-tweet (fn [new-tweet] (go (>! tweets new-tweet)))]
+      (tweet/connect-tweet-server thread-pool 4000 update-tweet)))
 
-; start connection
-(def connection (tweet/connect-tweet-server thread-pool 4000 update-tweet))
-
-; for live coding and testing
-;(tweet/stop-connection connection)
-
-(defn image-available?
-  "checks if requested PImage is available (for use with q/request-image)"
-  [pImage]
-  (and (not (nil? pImage)) (> (.height pImage) 0)))
+  (defn- draw []
+    (let [tweet (<!! tweets)
+          text (:text tweet)
+          screen-name (get-in tweet [:user :screen_name])
+          image-url (get-in tweet [:user :profile_image_url])]
+      ; Clear the sketch by filling it with black
+      (q/background 0)
+      ; set fill color (text color)
+      (q/fill 230)
+      ; draw text
+      (do
+        (q/text text 60 0 200 800)
+        (q/text (str \@ screen-name) 280 10)))))
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  ; setup function returns initial state.
-  {:old_id_str ""
-   :profile-image nil})
-
-(defn update [state]
-  ; Update state map
-  ; if @tweet has been updated inject new fields in state map
-  ; and request new profile image
-  (let [state_id_str (:old_id_str state)
-		tweet_id_str (:id_str @tweet)
-		new-tweet? (not (= state_id_str tweet_id_str))]
-  	{:old_id_str (if new-tweet? tweet_id_str state_id_str)
-   	 :text (:text @tweet)
-   	 :screen-name (:screen-name @tweet)
-   	 :profile-image (if new-tweet? (-> @tweet :profile-image-url q/request-image) (:profile-image state))}))
-
-(defn draw [{:keys [:text :screen-name :profile-image] :as state}]
-  ; Clear the sketch by filling it with black
-  (q/background 0)
-  ; set fill color (text color)
-  (q/fill 230)
-  ; draw text
-  (if text
-	(do
-		(q/text text 60 0 200 800)
-		(q/text (str \@ screen-name) 280 10)))
-  ; draw profile-image if available
-  (if (image-available? profile-image) (q/image profile-image 0 0)))
+  (start-connection))
 
 (q/defsketch hello-quil
   :title "tweet display hello world"
   :size [500 500]
   ; setup function called only once, during sketch initialization.
   :setup setup
-  ; update is called on each iteration before draw is called.
-  ; It updates sketch state.
-  :update update
   :draw draw
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
+  )
